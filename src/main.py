831335d2fcf30749846b6807dccb7e014437fc07
voltrecto <voltairerecto@gmail.com>
2025-03-23 19:15:41 -0500

    Add more pages

--- src/main.py
+++ src/main.py
@@ -18,13 +18,29 @@ def copy_static(source, destination):
         else:
             copy_static(source_path, destination_path)
 
+def generate_all_markdown(source_dir, destination_dir, template_path):
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.endswith(".md"):
+                from_path = os.path.join(root, file)
+                dest_path = from_path.replace(source_dir, destination_dir).replace(".md", ".html")
+
+                dest_dir = os.path.dirname(dest_path)
+                if not os.path.exists(dest_dir):
+                    os.makedirs(dest_dir)
+                
+                print(f"Generating HTML for: {from_path}")
+                print(f"Output file: {dest_path}")
+                generate_page(from_path=from_path, template_path=template_path, dest_path=dest_path)
+
+
 def main():
     print("Starting static site generation...")
     if os.path.exists("public"):
         shutil.rmtree("public")
     os.makedirs("public", exist_ok=True)
     copy_static("static", "public")
-    generate_page(from_path="content/index.md", template_path="template.html", dest_path="public/index.html")
+    generate_all_markdown(source_dir="content", destination_dir="public", template_path="template.html")
     print("Static site generation complete.")
 
 if __name__ == "__main__":
